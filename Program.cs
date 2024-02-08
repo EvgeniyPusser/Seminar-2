dd8994f4e1957a2d67631850c7100ba693096087
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-09 01:52:35 +0300

    Tasks

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// System.Console.Write("Введите число");
+//  int number1 = Convert.ToInt32(Console.ReadLine());
+
+//  if(number1 % 7 ==0  && number1 % 23 == 0)
+//  {
+//    System.Console.WriteLine("ДА");
+//  }
+//  else
+//  {
+//    System.Console.WriteLine("НЕТ");
+//  }
+
+
+System.Console.Write("Введите координату x числа");
+ int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите координату y числа");
+ int y = Convert.ToInt32(Console.ReadLine());
+
+ if(x > 0)
+ {
+   if(y > 0)
+   {
+    System.Console.WriteLine("Первая координатная четверть");
+   }
+   else
+   {
+    System.Console.WriteLine("Четёртая координатная четверть");
+   }
+ }
+ else
+ {
+   if(y > 0){
+     System.Console.WriteLine("Вторая координатная четверть");
+   }
+   else
+   {
+     System.Console.WriteLine("Третья координатная четверть");
+   }
+ }
+
